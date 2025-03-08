1ba1b8784ed51e8701ff5ba25260e7b4574dfa69
Federico De Donatis <92383436+FedericoDD@users.noreply.github.com>
2025-03-08 18:25:03 +0100

    Refactor MEKF implementation to add a fast version and update state vector dimensions

--- PostProcess/DataAnal/MEKF.py
+++ PostProcess/DataAnal/MEKF.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sigma_mag ,x_k_1, P_k_1):
+def MEKF_fast(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sigma_mag ,x_k_1, P_k_1):
     # dt: time interval (use gnss time)
     # acc_meas: 3 by 1, Body frame
     # gyro_meas: 3 by 1, Body frame
@@ -9,7 +9,7 @@ def MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sig
     # x_k_1: 14 by 1, 
         # [0:4] quaternion
         # [4:7] gyro bias
-        # [7:13] delta X
+        # [7:13] delta X (delta rho, delta beta)
     # P_k_1: 13 by 13, covariance matrix
 
     
@@ -112,6 +112,109 @@ def MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sig
     
     return x_k, P_k
 
+def MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc_ARW, sigma_acc_RRW, sigma_ARW, sigma_RRW, sigma_mag ,x_k_1, P_k_1):
+    # dt: time interval (use gnss time)
+    # acc_meas: 3 by 1, Body frame
+    # gyro_meas: 3 by 1, Body frame
+    # mag_meas: 3 by 1, Body frame
+    # R_BODY2ENU: 3 by 3, rotation matrix from body frame to ENU frame
+    # x_k_1: 14 by 1, state vector:
+        # [0:3] orientation error
+        # [3:6] velocity error
+        # [6:9] position error
+        # [9:12] gyro bias
+        # [12:15] accelerometer bias
+        # [15:18] magnetometer bias
+    # P_k_1: 13 by 13, covariance matrix
+
+    
+    I3 = np.eye(3)
+    I4 = np.eye(4)
+    I18 = np.eye(18)
+
+    # Propagated value from previous step
+    q_k = x_k_1[0:4]
+    beta_gyro_k = x_k_1[9:12]
+    beta_acc_k = x_k_1[12:15]
+    beta_mag_k = x_k_1[15:18]
+    x_k = np.zeros(18)
+
+    # Predition
+    # Depolarize bias from gyroscope
+    omega_k = gyro_meas - beta_gyro_k
+    a_k = acc_meas - beta_acc_k
+    m_k = mag_meas - beta_mag_k
+
+    # Quaternion propagation
+    Omega_k = np.array([[0, omega_k[2], -omega_k[1], omega_k[0]],
+            [-omega_k[2], 0, omega_k[0], omega_k[1]],
+            [omega_k[1], -omega_k[0], 0, omega_k[2]],
+            [-omega_k[0], -omega_k[1], -omega_k[2], 0]])
+    q_k = np.dot(I4 + 0.5*dt*Omega_k, q_k)
+    q_k = q_k/np.linalg.norm(q_k)
+
+    # Covariance equation propagation
+    R_k = Quaternion2Rotation(q_k).transpose()
+    G = np.zeros((18,18))
+    G[0:3,9:12] = -I3
+    G[6:9,3:6] = I3
+    G[0:3,0:3] = - skew_symmetric(omega_k)
+    G[3:6,0:3] = - np.dot(R_k, skew_symmetric(a_k))
+    G[3:6, 12:15] = - R_k
+    F = np.eye(18) + G*dt
+    Q = np.zeros((18, 18))
+    Q[0:3, 0:3] = (sigma_ARW**2*dt + 1/3 * sigma_RRW**2*dt**3)*I3
+    Q[0:3, 9:12] = -0.5*sigma_RRW**2*dt**2*I3
+    Q[3:6, 3:6] = (sigma_acc_ARW**2*dt + 1/3 * sigma_acc_RRW**2*dt**3)*I3
+    Q[3:6, 6:9] = sigma_acc_RRW**2*I3*(dt**4)/8.0 + sigma_acc_ARW**2*I3*(dt**2)/2.0
+    Q[3:6, 12:15] = -sigma_acc_RRW**2*I3*(dt**2)/2.0
+    Q[6:9, 3:6] = sigma_acc_ARW**2*I3*(dt**2)/2.0 + sigma_acc_RRW**2*I3*(dt**4)/8.0
+    Q[6:9, 6:9] = sigma_acc_ARW**2*I3*(dt**3)/3.0 + sigma_acc_RRW**2*I3*(dt**5)/20.0
+    Q[6:9, 12:15] = -sigma_acc_RRW**2*I3*(dt**3)/6.0
+    Q[9:12, 0:3] = -sigma_RRW**2*(dt**2)/2.0
+    Q[9:12, 9:12] = sigma_RRW**2*dt
+    Q[12:15, 3:6] = -sigma_acc_RRW**2*I3*(dt**2)/2.0
+    Q[12:15, 6:9] = -sigma_acc_RRW**2*I3*(dt**3)/6.0
+    Q[12:15, 12:15] = sigma_acc_RRW**2*I3*dt
+    Q[15:18, 15:18] = sigma_mag**2*dt
+    
+    P_k = np.dot(F, np.dot(P_k_1, F.transpose())) + Q
+
+    # Update
+    H = np.zeros((6,18))
+    H[0:3, 0:3] = skew_symmetric(np.dot(R_k, np.array([0,0,-1])))
+    H[0:3, 12:15] = I3
+    H[0:3, 12:15] = skew_symmetric(np.dot(R_k, np.array([1,0,0])))
+    H[3:6, 15:18] = I3
+    R = np.identity(6)
+    R[0:3, 0:3] = sigma_acc_ARW * np.identity(3)
+    R[3:6, 3:6] = sigma_mag * np.identity(3)
+    # Kalman gain
+    K = np.dot(np.dot(P_k, H.transpose()), np.linalg.inv(np.dot(H, np.dot(P_k, H.transpose())) + R))
+    
+    # Update covariance
+    P_k = np.dot(np.dot((I18 - np.dot(K, H)), P_k), (I18 - np.dot(K, H)).transpose()) + np.dot(K, np.dot(R, K.transpose()))
+    
+    observation = np.zeros(shape=(6, ))
+    observation[0:3] = a_k
+    observation[3:6] = m_k
+    predicted_observation = np.zeros(shape=(6, ), dtype=float)
+    predicted_observation[0:3] = np.dot(R_k, np.array([0,0,-1]))
+    predicted_observation[3:6] = np.dot(R_k, np.array([1,0,0]))
+    x_k = np.dot(K, (observation - predicted_observation))
+
+    # Update x_k
+    drho = 0.5*x_k[0:3]
+    q_4 = np.sqrt(1 - np.dot(drho, drho))
+    d_q = np.array([drho[0],drho[1],drho[2], q_4])
+    x_k[0:4] = QuaternionProduct(q_k,d_q)
+    x_k[9:12] = beta_gyro_k + x_k[9:12]
+    x_k_1[12:15] = beta_acc_k + x_k[12:15]
+    x_k_1[15:18] = beta_mag_k + x_k[15:18]
+
+    
+    return x_k, P_k
+
 def Quaternion2Rotation(q):
     # q: 4 by 1, quaternion
     # R: 3 by 3, rotation matrix from body to ENU
@@ -144,19 +247,10 @@ def QuaternionProduct(q1_v, q2_v):
     q = q/np.linalg.norm(q)
     return q
 
-'''
-def QuaternionProduct(q1_v, q2_v):
-    # q1, q2: 4 by 1, quaternion
-    # q: 4 by 1, quaternion
-    qv1 = q2_v[0:3]
-    qv2 = q2_v[0:3]
-    qs1 = q1_v[3]
-    qs2 = q2_v[3]
-    q=np.zeros((4,))
-    q[0:3] = np.array([qv1*qs2 + qs1*qv2 - np.cross(qv1, qv2)])
-    print(q[0:3])
-    q[3] = qs1*qs2 - np.dot(qv1, qv2)
-    
-    q = q/np.linalg.norm(q)
-    return q
-'''
+def skew_symmetric(v):
+    # v: 3 by 1, vector
+    # v_skew: 3 by 3, skew symmetric matrix
+    v_skew = np.array([[0, -v[2], v[1]],
+                        [v[2], 0, -v[0]],
+                        [-v[1], v[0], 0]])
+    return v_skew
--- PostProcess/main.py
+++ PostProcess/main.py
@@ -13,32 +13,63 @@ data = pp.import_data(FILE_NAME)
 
 # MEKF
 # Initial state
-x_k = np.zeros(13)
+x_k = np.zeros(18)
 x_k[0:4] = data.iloc[0, 10:14].values
-P_k = np.eye(6)*1e-12
+P_k = np.eye(18)*1e-12
 
-sigma_acc = 0.4
+sigma_acc_ARW = 1
+sigma_acc_RRW = 0.5
 sigma_ARW = 0.66 * np.pi / 180 / 3600  # rad/sqrt(s)
-sigma_RRW = 0.4 * np.pi / 180 / 3600 # rad/s
-sigma_mag = 1.4e3
-bias = np.array([0,0,np.linalg.norm(data.iloc[0,1:4])])
+sigma_RRW = 0.4 * np.pi / 180 / 3600  # rad/s
+sigma_mag = 1.1e3 * 3
 
 q_vec = np.zeros((len(data),4))
 q_vec[0, :] = x_k[0:4]
+x_vect = np.zeros((len(data),3))
+x_vect[0, :] = np.zeros(3,)
+vel = np.zeros(3,)
+acc_bias = np.array([0,0,np.linalg.norm(data.iloc[1,1:4])])
 for i in range(1, len(data)):
     R = mekf.Quaternion2Rotation(x_k[0:4])
     dt = data.iloc[i, 0] - data.iloc[i-1, 0]
-    acc_meas = data.iloc[i, 1:4].values - np.dot(R.transpose(), bias)
+    acc_meas = data.iloc[i, 1:4].values 
     gyro_meas = data.iloc[i, 4:7].values
     mag_meas = data.iloc[i, 7:10].values
     
     x_k = x_k
     P_k = P_k
 
-    x_k, P_k = mekf.MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sigma_mag, x_k, P_k)
+    x_k, P_k = mekf.MEKF(dt, acc_meas, gyro_meas, mag_meas, sigma_acc_ARW, sigma_acc_RRW, sigma_ARW, sigma_RRW, sigma_mag, x_k, P_k)
     
     # save x_k to data
     q_vec[i, :] = x_k[0:4]
+    acc_clean = acc_meas - np.dot(R.transpose(), acc_bias)
+    x_vect[i, :] =  x_vect[i-1, :] + acc_clean*dt**2
+
+# MEKF
+# Initial state
+x_k = np.zeros(13)
+x_k[0:4] = data.iloc[0, 10:14].values
+P_k = np.eye(6)*1e-12
+
+sigma_acc = sigma_acc_ARW
+
+q_vec_fast = np.zeros((len(data),4))
+q_vec_fast[0, :] = x_k[0:4]
+for i in range(1, len(data)):
+    R = mekf.Quaternion2Rotation(x_k[0:4])
+    dt = data.iloc[i, 0] - data.iloc[i-1, 0]
+    acc_meas = data.iloc[i, 1:4].values
+    gyro_meas = data.iloc[i, 4:7].values
+    mag_meas = data.iloc[i, 7:10].values
+    
+    x_k = x_k
+    P_k = P_k
+
+    x_k, P_k = mekf.MEKF_fast(dt, acc_meas, gyro_meas, mag_meas, sigma_acc, sigma_ARW, sigma_RRW, sigma_mag, x_k, P_k)
+    
+    # save x_k to data
+    q_vec_fast[i, :] = x_k[0:4]
 
 
 # plot result
@@ -46,13 +77,33 @@ plt.figure()
 plt.subplot(4, 1, 1)
 plt.plot(data.iloc[:, 0], data.iloc[:, 10], label='v1')
 plt.plot(data.iloc[:, 0], q_vec[:, 0], label='qm1')
+plt.plot(data.iloc[:, 0], q_vec_fast[:, 0], label='qm1')
 plt.subplot(4, 1, 2)
 plt.plot(data.iloc[:, 0], data.iloc[:, 11], label='v2')
 plt.plot(data.iloc[:, 0], q_vec[:, 1], label='qm2')
+plt.plot(data.iloc[:, 0], q_vec_fast[:, 1], label='qm2')
 plt.subplot(4, 1, 3)
 plt.plot(data.iloc[:, 0], data.iloc[:, 12], label='v3')
 plt.plot(data.iloc[:, 0], q_vec[:, 2], label='qm3')
+plt.plot(data.iloc[:, 0], q_vec_fast[:, 2], label='qm3')
 plt.subplot(4, 1, 4)
 plt.plot(data.iloc[:, 0], data.iloc[:, 13], label='4')
 plt.plot(data.iloc[:, 0], q_vec[:, 3], label='qm4')
+plt.plot(data.iloc[:, 0], q_vec_fast[:, 3], label='qm4')
+plt.legend(['q_IMU', 'q_MEKF', 'q_MEKF_fast'])
+plt.show()
+
+plt.figure()
+plt.subplot(3, 1, 1)
+plt.plot(data.iloc[:, 0], x_vect[:, 0], label='x')
+plt.subplot(3, 1, 2)
+plt.plot(data.iloc[:, 0], x_vect[:, 1], label='y')
+plt.subplot(3, 1, 3)
+plt.plot(data.iloc[:, 0], x_vect[:, 2], label='z')
+plt.show()
+
+# 3D plot 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot(x_vect[20:, 0], x_vect[20:, 1], x_vect[20:, 2])
 plt.show()
